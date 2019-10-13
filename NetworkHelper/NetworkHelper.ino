2740a31864a43094fd3cd2e5196bf0c7b65afcf2
Sam Stratter <humanharddrive@gmail.com>
2019-10-12 22:32:16 -0400

    Configure AP. Create login page

--- NetworkHelper/NetworkHelper.ino
+++ NetworkHelper/NetworkHelper.ino
@@ -7,9 +7,9 @@
 #define DEBUG
 
 //Access point configuration
-IPAddress local_IP(192,168,1,1);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
+IPAddress local_IP(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
 
 const char* sHelperNetworkServerName = "networkhelper";
 const char* sHelperNetworkSSID = "esp8266";
@@ -19,12 +19,31 @@ ESP8266WebServer server(80);
 
 void handleRoot()
 {
- server.send(200, "text/html", "<form action=\"/login\" method=\"POST\"><input type=\"text\" name=\"username\" placeholder=\"Username\"></br><input type=\"password\" name=\"password\" placeholder=\"Password\"></br><input type=\"submit\" value=\"Login\"></form><p>Try 'John Doe' and 'password123' ...</p>");
+  server.send(200, "text/html", "<form action=\"/NetworkChange\" method=\"POST\"><input type=\"text\" name=\"ssid\" placeholder=\"SSID\"></br><input type=\"password\" name=\"password\" placeholder=\"Password\"></br><input type=\"submit\" value=\"Update\"></form>");
+}
+
+void handleNetworkChange()
+{
+  if (!server.hasArg("ssid") || !server.hasArg("password") ||
+      !server.arg("ssid") || !server.arg("password"))
+  {
+    server.send(400, "text/plain", "400: Invalid Request");
+  }
+  else
+  {
+#ifdef DEBUG
+    Serial.println("Connect to...");
+    Serial.print("SSID: "); Serial.println(server.arg("ssid"));
+    Serial.print("Password: "); Serial.println(server.arg("password"));
+#endif
+    server.send(200, "text/html", "<h1>Welcome!</h1><p>Login successful</p>");
+  }
 }
 
 void configureServer(ESP8266WebServer& server)
 {
   server.on("/", handleRoot);
+  server.on("/NetworkChange", HTTP_POST, handleNetworkChange);
   MDNS.begin(sHelperNetworkServerName);
 }
 
