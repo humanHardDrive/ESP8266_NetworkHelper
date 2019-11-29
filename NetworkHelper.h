082205e42f55168ee48284fab05f6da437fbf1d3
Sam Stratter <humanharddrive@gmail.com>
2019-11-29 13:28:27 -0500

    Add stuff for MQTT connnection behind #def

--- NetworkHelper.h
+++ NetworkHelper.h
@@ -8,6 +8,10 @@
 
 #include <string>
 
+#ifdef MQTTHelper
+#define MQTT_STATUS_OFFSET	-4
+#endif
+
 class NetworkHelper
 {
 public:
@@ -23,20 +27,32 @@ public:
 	void stop();
 
 	void onNetworkChange(std::function<void(String, String)> fn) { m_OnNetworkChange = fn; }
+#ifdef MQTTHelper
+	void onServerChange(std::function<void(String, uint16_t, String, String)> fn) { m_OnServerChange = fn; }
+#endif
 
 private:
 	void configureServer();
 
 	void handleRoot();
+	
 	void handleManualEntry();
 	void handleScan();
 	void handleNetworkChange();
+
+#ifdef MQTTHelper
+	void handleServerEntry();
+	void handleTestServerConnection();
+#endif
 
 	ESP8266WebServer m_Server;
 	String m_sServerName;
 	bool m_bRunning = false;
 
 	std::function<void(String, String)> m_OnNetworkChange;
+#ifdef MQTTHelper
+	std::function<void(String, uint16_t, String, String)> m_OnServerChange;
+#endif
 
 	const String ENCRYPTION_NAME[9] =
 	{
@@ -50,6 +66,22 @@ private:
 		"NONE",			//7
 		"WPA/WPA2/PSK"	//8
 	};
+	
+#ifdef MQTTHelper
+	const String MQTT_STATUS_NAME[10]
+	{
+		"CONNECTION TIMEOUT",		//-4
+		"CONNECTION LOST",			//-3
+		"CONNECT FAILED",			//-2
+		"DISCONNECTED",				//-1
+		"CONNECTED",				//0
+		"BAD PROTOCOL",				//1
+		"BAD CLIENT ID",			//2
+		"UNAVAILABLE",				//3
+		"BAD CREDENTIALS",			//4
+		"UNAUTHORIZED"				//5
+	};
+#endif
 };
 
 #endif
